a789f825bc8a3c32b0bb36ab3ad8a6dcc1a83100
Danial Manavi <dmanavi@live.com>
2025-02-04 22:43:58 -0800

    chore: added X-Forwarded-For documentation to the API throttling

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -20,6 +20,9 @@ import { UserModule } from './user.module';
     // And the rate limiting information is not shared between multiple instances of the application.
     // If you want to share the rate limiting information between multiple instances of the application,
     // you can use a Redis store (I couldn't find a good implementation and implementing is out of this demo project scope).
+
+    // DM: API configuration is configured to work behind a reverse proxy.
+    // This requires the application to trust the proxy and to use the X-Forwarded-For header to get the client's IP address.
     ThrottlerModule.forRoot({
       throttlers: [
         {
